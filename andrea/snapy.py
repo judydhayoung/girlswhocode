6dc9ad37f61795a74fd633982c41bc0a0e411a5b
gwc22 <gwc22@csad.cs.calvin.edu>
2018-10-01 20:16:21 -0400

    andrea's turtles!

---
+++ andrea/snapy.py
@@ -0,0 +1,12 @@
+import turtle
+
+snapy = turtle.Turtle()
+snapy.shape("turtle") 
+snapy.pendown()
+snapy.forward(200)
+snapy.left(90)
+snapy.forward(100)
+snapy.left(90)
+snapy.forward(200)
+snapy.left(90)
+snapy.forward(100)
