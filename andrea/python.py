5aedc27e66127d21058e03230838d40a10ef9603
gwc22 <gwc22@csad.cs.calvin.edu>
2018-10-15 20:09:16 -0400

    andrea's calculate.py

---
+++ andrea/python.py
@@ -0,0 +1,34 @@
+def caculate (num1, num2):
+    result = num1+num2
+    print("result = "+ str(result))
+
+number1 = int(input("Enter a number:"))
+number2 = int(input("Enter a number:"))
+caculate(number1, number2)
+
+def multiplication (num1, num2):
+    result = num1 * num2
+    print("result = "+ str(result))
+
+
+number1 = int(input("Enter a number:"))
+number2 = int(input("Enter a number:"))
+multiplication(number1, number2)
+
+def divison (num1, num2):
+    result = num1 / num2
+    print("result = "+ str(result))
+
+
+number1 = int(input("Enter a number:"))
+number2 = int(input("Enter a number:"))
+divison(number1, number2)
+
+def subtraction (num1, num2):
+    result = num1 - num2
+    print("result = "+ str(result))
+
+
+number1 = int(input("Enter a number:"))
+number2 = int(input("Enter a number:"))
+subtraction(number1, number2)
