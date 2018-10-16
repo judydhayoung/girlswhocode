aabe0fa655757496ab2bfe6a1ac097e617f1e95d
kohtzemay <dearmegan@outlook.com>
2018-10-15 20:26:49 -0400

    mariah's calculate.py

---
+++ mariah/calulate.py
@@ -0,0 +1,17 @@
+def calculate(num1, num2):
+    result= num1 + num2
+    print("result = " + str(result))
+
+number1 = int(input("Enter a number: "))
+number2 = int(input("enter a 2nd number"))
+calculate(number1,number2)
+
+def multiplication(num1, num2):
+    result= num1 * num2
+    print("result = " + str(result))
+
+number1 = int(input("Enter a number: "))
+number2 = int(input("enter a 2nd number"))
+multiplication(number1,number2)
+
+
