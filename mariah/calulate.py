9eae77dc3e9acb549e90fb02921b5bfff5866fa0
kohtzemay <dearmegan@outlook.com>
2018-10-22 20:03:16 -0400

    mariah's calculate

--- mariah/calulate.py
+++ mariah/calulate.py
@@ -1,17 +1,48 @@
 def calculate(num1, num2):
+
     result= num1 + num2
+
     print("result = " + str(result))
 
-number1 = int(input("Enter a number: "))
-number2 = int(input("enter a 2nd number"))
-calculate(number1,number2)
+
 
 def multiplication(num1, num2):
+
     result= num1 * num2
+
     print("result = " + str(result))
 
-number1 = int(input("Enter a number: "))
-number2 = int(input("enter a 2nd number"))
-multiplication(number1,number2)
 
 
+def subtraction(num1, num2):
+
+    result= num1 - num2
+
+    print("result = " + str(result))
+
+
+
+def division(num1, num2):
+
+    result= num1 / num2
+
+    print("result = " + str(result))
+
+
+
+def main():
+    print("what operation do want to do?\n 1.Addition \n 2. subtraction \n3. Multiplication \n 4. Division\n")
+    choice=int(input("Enter number of operation"))
+    first=int(input("Enter first number to calulate"))
+    second=int(input("Enter second number to calulate"))
+    if choice=='1':
+        addition(first,second)
+    elif choice==2:
+        subtraction(first,second)
+    elif choice==3:
+        multiplication(first,second)
+    elif choice==4:
+        divison(first,second)
+
+main()
+               
